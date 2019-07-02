2b6c942271ce9d3219a2dc30fdb99459d5608113
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-07-02 11:17:39 -0400

    Fixed the bug that caused the podcast div to appear twice upon clicking the submit button. Removed javascript link to file that was no longer in use. Changed card styling to allow maps and images to take the entire body of the card

--- assets/javascript/mainpage.js
+++ assets/javascript/mainpage.js
@@ -160,12 +160,12 @@ $( document ).ready(function() {
     }
 
 
-    $(document).on("click", "#submit-button", displayPods);
+//     $(document).on("click", "#submit-button", displayPods);
     $(document).on("click", "#submit-button", displayMap);
     $(document).on("click", "#submit-button", displayTime);
     //$(document).on("click", "#submit-button", displayPods);
     $("#submit-button").on("click", function(){
-       setTimeout(displayPods, 1000);
+       setTimeout(displayPods, 1000); 
     });
 
 
@@ -222,39 +222,7 @@ $( document ).ready(function() {
 
            }
 
-    // var inlocation = $('#location').val();
-    // //console.log("Starting Location:" +inlocation);
-    // var indestination = $('#destination').val();
-    // //console.log("Your Destination:" +indestination);
-
-    // if ((inlocation == 0) && (indestination == 0))
-    //      {
-    //          // Starting Location and Destination are blank
-    //         // console.log("MISSING THE LOCATION");
-    //         // console.log("Both missing - Starting Location:" +inlocation);
-    //         // console.log("Both missing - Your Destination:" +indestination);
-    //          $('audio#pop1')[0].play()
-    //          //$('#location').val('Location cannot be blank').css('color', 'red');
-    //         // $('#destination').val('Destination cannot be blank').css('color', 'red');
-    //      }
-    //  else
-    //      if ((inlocation == 0) && (indestination !== 0))
-    //           {
-    //                // Starting Location is blank and Destination is not
-    //               // console.log("Just Location missing - Starting Location:" +inlocation);
-    //                $('audio#pop2')[0].play()
-    //           //     $('#location').val('Location cannot be blank').css('color', 'red');
-    //           }
-    //      else
-    //         if ((indestination == 0) && (inlocation !== 0))
-    //                {
-    //                     //  Destination is blank and Starting Location is not
-    //                    // console.log("MISSING THE DESTINATION");
-    //                     //console.log("Just Dest missing - Your Destination:" +indestination);
-    //                     $('audio#pop3')[0].play()
-    //                    // $('#destination').val('Destination cannot be blank').css('color', 'red');
-    //                 } 
-                //    }
+   
 })
 
 
